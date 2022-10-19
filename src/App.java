2a0a04015a0693b5c97ed486168c54fa140f5352
Jtorrpab <juanpablotorresp.17@gmail.com>
2022-10-19 18:43:17 -0500

    Add connection branch

--- src/App.java
+++ src/App.java
@@ -1,5 +1,20 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class App {
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        try {
+            
+            String url = "jdbc:sqlite:hr.db";
+            Connection conn = DriverManager.getConnection(url);
+            if(conn != null){
+                System.out.println("Conexión exitosa");
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+
     }
 }
