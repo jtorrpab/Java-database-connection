b872545566e5b039501afbf7d3f3720b5893b494
Jtorrpab <juanpablotorresp.17@gmail.com>
2022-10-19 19:40:00 -0500

    primer consumoDB

--- src/App.java
+++ src/App.java
@@ -1,23 +1,34 @@
-import java.beans.Statement;
 import java.sql.Connection;
 import java.sql.DriverManager;
+//import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+
 
 public class App {
 
     public static void main(String[] args) throws Exception {
     
-        ConexionDB();
-    }
+    //     ConexionDB();
+    // }
 
-    public static void ConexionDB(){
+    // public static void ConexionDB(){
         try {
-            String url = "jdbc:sqlite:hr.db";
-            Connection conn = DriverManager.getConnection(url);
-            if(conn != null){
-                System.out.println("Conexión exitosa");
+            Connection conn = DriverManager.getConnection("jdbc:sqlite:hr.db");
+            if (conn != null) {
+                System.out.println("Conexion exitosa");
+                Statement st = conn.createStatement();
+
+                //Consumo de la base de datos
+                ResultSet res = st.executeQuery("SELECT * FROM employees");
+                while(res.next()){
+                    System.out.println("Nombre "+res.getString("first_name"));
+                }
+                //System.out.println(res);
             }
         } catch (SQLException e) {
+            e.printStackTrace();
             System.out.println(e.getMessage());
         }
     }
